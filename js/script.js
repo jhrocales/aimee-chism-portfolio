e4c60a41fa0b37e5405455ea76bb3bfe14cfeb09
jhrocales <jhrocales@gmail.com>
2025-12-22 18:07:45 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// typing animation
+
+var typed = new Typed(".typing", {
+  strings: ["", "Graphic Designer", "Email Designer,", "Video Editor"],
+  typeSpeed: 80,
+  BackSpeed: 60,
+  loop: true,
+});
